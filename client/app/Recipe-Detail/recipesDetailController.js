be49850fc61b9e7d8d8d84d567c6c70c1135fc1e
Jacob Boland <jacob.boland@gmail.com>
2014-12-28 17:20:12 -0800

    Add use strict and remove scope.id from recipesDetailController

--- client/app/Recipe-Detail/recipesDetailController.js
+++ client/app/Recipe-Detail/recipesDetailController.js
@@ -1,7 +1,7 @@
 angular.module('ShannosRasoi').controller('recipesDetailController', ['$scope', '$stateParams', 'recipesService', function($scope, $stateParams, recipesService) {
-	$scope.id = $stateParams.id;
-	recipesService.getRecipe($scope.id).then(function(recipe){
+	'use strict';
+
+	recipesService.getRecipe($stateParams.id).then(function(recipe){
  		$scope.recipe = recipe || {};
 	});
-
 }]);
