71b826fdde67782f5f5ea83f7bc65d6a3ed61462
boskya <bosky.atlani@gmail.com>
2016-02-21 21:50:06 -0800

    adding less and semantic ui

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -2,19 +2,28 @@ var gulp = require('gulp');
 var webpack = require('gulp-webpack');
 var watch = require('gulp-watch');
 var runseq = require('run-sequence');
+var less = require('gulp-less');
+var path = require('path');
+var LessPluginCleanCSS = require('less-plugin-clean-css'),
+    LessPluginAutoPrefix = require('less-plugin-autoprefix'),
+    cleancss = new LessPluginCleanCSS({ advanced: true }),
+    autoprefix = new LessPluginAutoPrefix({ browsers: ["last 2 versions"] });
+
+gulp.task('less', function() {
+  gulp.src('src/assets/less/**/theme.config')
+  .pipe(gulp.dest('node_modules/semantic-ui-less/'));
+
+  gulp.src('src/assets/less/**/*.less')
+    .pipe(less({
+      plugins: [autoprefix, cleancss],
+      paths: [ path.join(__dirname, 'node_modules') ]
+    }))
+    .pipe(gulp.dest('dist/css'));
+});
 
 gulp.task('copyhtml', function() {
    gulp.src('src/**/*.html')
    .pipe(gulp.dest('dist/'));
-});
-
-gulp.task('watch', function() {
-  gulp.watch('src/**/*.js', function() {
-    runseq('webpack');
-  });
-  gulp.watch('src/**/*.html',  function() {
-       runseq('copyhtml');
-  });
 });
 
 gulp.task('webpack', function() {
@@ -23,4 +32,17 @@ gulp.task('webpack', function() {
     .pipe(gulp.dest('dist/'));
 });
 
-gulp.task('default', ['copyhtml', 'webpack']);
+gulp.task('watch', function() {
+  gulp.watch('src/**/*.less', function() {
+    runseq('less');
+  });
+
+  gulp.watch('src/**/*.js', function() {
+    runseq('webpack');
+  });
+  gulp.watch('src/**/*.html',  function() {
+       runseq('copyhtml');
+  });
+});
+
+gulp.task('default', ['copyhtml', 'webpack', 'less']);
--- client/src/main.js
+++ client/src/main.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom';
 
 class Hello extends React.Component {
   render() {
-    return <h1>Hello</h1>
+    return <div className="ui three item menu">
+              <a className="active item">Editorials</a>
+              <a className="item">Reviews</a>
+              <a className="item">Upcoming Events</a>
+          </div>
   }
 }
 
